8427d9d8d4c86fc1ef1e8e3202eec88160019dfe
mapleAndwoman <fangshengzhou@hisilicon.com>
2021-05-15 21:41:21 +0800

    Description: first rust program @home

---
+++ rust/hello_world/target/debug/deps/hello_world-24befa09d518f784.d
@@ -0,0 +1,5 @@
+/home/zx/learngit/rust/hello_world/target/debug/deps/hello_world-24befa09d518f784: src/main.rs
+
+/home/zx/learngit/rust/hello_world/target/debug/deps/hello_world-24befa09d518f784.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/zx/learngit/rust/hello_world/target/debug/hello_world: /home/zx/learngit/rust/hello_world/src/main.rs
